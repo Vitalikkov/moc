87b4301cf06bba7ca3235f70c1ff5a81dad4cfde
vitalik kovtiukh <106430549+Vitalikkov@users.noreply.github.com>
2024-04-05 23:49:13 +0300

    sdds

--- src/app/component/card.tsx
+++ src/app/component/card.tsx
@@ -1,24 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Image from "next/image";
 import "./card.scss";
-import axios from 'axios';
 
 const ProductCard = () => {
-  const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    axios.get('https://api.escuelajs.co/api/v1/products')
-      .then(response => {
-        setProducts(response.data);
-      })
-      .catch(error => {
-        console.error('Помилка отримання даних з API:', error);
-      });
-  }, []);
 
   return (
     <div className="product-container">
-      
+      <p>adadsasdadsd</p>
       
     </div>
   );

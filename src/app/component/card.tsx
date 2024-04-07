1463abec9c4d5a8de1c6fc775dbd791397732fc5
vitalik kovtiukh <106430549+Vitalikkov@users.noreply.github.com>
2024-04-07 14:14:09 +0300

    add

--- src/app/component/card.tsx
+++ src/app/component/card.tsx
@@ -1,17 +1,56 @@
-import React from "react";
-import Image from "next/image";
+
+'use client'
+
+import React, { useState, useEffect } from 'react';
 import "./card.scss";
 
-const ProductCard = () => {
+
+const Card = () => {
+  const [cardItems, setСardItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
 
-  return (
-    <div className="product-container">
-      <p>adadsasdadsd</p>
-      
-    </div>
-  );
-}
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.escuelajs.co/api/v1/products');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const data = await response.json();
+        setСardItems(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
 
-export default ProductCard;
+    fetchData();
+  }, []);
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+    return (
+      <div className="card-container">
+        {cardItems.map(item => (
+          <div key={item.id} className="card">
+            <h2>{item.title}</h2>
+            <p>Category: {item.category.name}</p>
+            <p>Description: {item.description}</p>
+            <p>Price: ${item.price}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+
+export default Card;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import ProductCard from "./component/card";
+import Card from "./component/card";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ProductCard/>
+      <Card/>
     </main>
   );
 }
